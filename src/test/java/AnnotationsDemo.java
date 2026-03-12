22f4ff39f1c15a24614c9f55f25ca03c2a242c4d
yazhinijeeva@gmail.com <yazhinijeeva@gmail.com>
2026-03-12 21:28:23 +0800

    FirstCommit

---
+++ src/test/java/AnnotationsDemo.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AnnotationsDemo {
+	WebDriver driver;
+	
+	void  LaunchApplication()
+	{
+		driver=new ChromeDriver();
+		driver.get("");
+		driver.findElement(By.id(null));
+	}
+	void test1()
+	{
+		
+	}
+	void test2()
+	{
+		
+	}
+	void test3()
+	{
+		
+	}
+
+}
